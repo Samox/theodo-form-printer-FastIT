51a78811ae07d3edeab9f00ed8331376a9162c25
Sammy Teillet <sammy.teillet@gmail.com>
2017-06-30 19:04:56 +0100

    Fix script

--- src/services/google.js
+++ src/services/google.js
@@ -1,6 +1,6 @@
 const gapi = window.gapi
 
-export const CLIENT_ID = '14597778711-d4kt5ktli45oohinerhi2u3l4rolbaoe.apps.googleusercontent.com	';
+export const CLIENT_ID = '14597778711-d4kt5ktli45oohinerhi2u3l4rolbaoe.apps.googleusercontent.com';
 export const DISCOVERY_DOCS = ["https://script.googleapis.com/$discovery/rest?version=v1"];
 export const SCOPES = 'https://www.googleapis.com/auth/forms';
 export const SCRIPT_ID_UK = 'MeRye5A_wHzCuyYvIJ85WNJ6eS0vCcuvm';
