0393fd4313a2bbed285973bd55dbf5c19c3d7b51
Sammy Teillet <sammy.teillet@gmail.com>
2017-07-03 14:26:03 +0100

    MOAR translations

--- src/components/ui/ProjectForm.jsx
+++ src/components/ui/ProjectForm.jsx
@@ -20,7 +20,7 @@ class ProjectForm extends PureComponent {
         <Icon name='print' />
       </Button>
       <Segment vertical>
-        <Header as="h3">How do you feel about the speed of the team?</Header>
+        <Header as="h3">Quelle est votre appréciation sur la vitesse d'avancement de l'équipe Theodo ?</Header>
         <Form.Radio label='5 - Excellente' checked={ this.props.form.speed ==='5 - Excellente' } />
         <Form.Radio label='4 - Très bonne' checked={ this.props.form.speed ==='4 - Très bonne' } />
         <Form.Radio label='3 - Bonne' checked={ this.props.form.speed ==='3 - Bonne' } />
@@ -29,7 +29,7 @@ class ProjectForm extends PureComponent {
         <Form.Radio label='0 - Très insuffisante' checked={ this.props.form.speed ==='0 - Très insuffisante' } />
       </Segment>
       <Segment vertical>
-        <Header as="h3">How do you feel about the quality of the collaboration with Theodo?</Header>
+        <Header as="h3">Quelle est votre appréciation sur la qualité de l'accompagnement Theodo ?</Header>
         <Form.Radio label='5 - Excellente' checked={ this.props.form.colaboration ==='5 - Excellente' } />
         <Form.Radio label='4 - Très bonne' checked={ this.props.form.colaboration ==='4 - Très bonne' } />
         <Form.Radio label='3 - Bonne' checked={ this.props.form.colaboration ==='3 - Bonne' } />
@@ -38,24 +38,24 @@ class ProjectForm extends PureComponent {
         <Form.Radio label='0 - Très insuffisante' checked={ this.props.form.colaboration ==='0 - Très insuffisante' } />
       </Segment>
       <Segment vertical>
-        <Header as="h3">What is the main priority the team should concentrate on to improve the score?</Header>
+        <Header as="h3">Quel changement/amélioration prioritaire pourrait vous amener à améliorer votre appréciation ?</Header>
         { this.props.form['client-voice'].split(/\n/).map((string, index) => <p key={`client-voice-${index}`}>{ string }</p>) }
       </Segment>
       <Segment vertical>
-        <Header as="h3">Should you have a magic wand, what is "the" thing you would change at Theodo?</Header>
+        <Header as="h3">Si vous aviez une baguette magique. quelle est "la" chose que vous changeriez chez Theodo ?</Header>
         { this.props.form['magic-wand'].split(/\n/).map((string, index) => <p key={`magix-wand-${index}`}>{ string }</p>) }
       </Segment>
       <Segment vertical>
-        <Header as="h3">Would you recommend Theodo?</Header>
+        <Header as="h3">Seriez-vous prêt à recommander Theodo ?</Header>
         <Form.Radio label='Oui bien sûr' checked={ this.props.form.recommendation === 'Oui bien sûr' } />
         <Form.Radio label='Oui' checked={ this.props.form.recommendation === 'Oui' } />
         <Form.Radio label='Pas vraiment' checked={ this.props.form.recommendation === 'Pas vraiment' } />
         <Form.Radio label='Pas du tout' checked={ this.props.form.recommendation === 'Pas du tout' } />
       </Segment>
       <Segment vertical>
-        <Header as="h3">Would you like to have a sales appointment with your Project Director this week?</Header>
+        <Header as="h3">Souhaitez-vous faire un point commercial avec Theodo dans la semaine à venir ?</Header>
         <Form.Radio label='Oui' checked={ this.props.form['sales-appointement'] === 'Oui' } />
-        <Form.Radio label='Non' checked={ this.props.form['sales-appointement'] === 'Non' } />
+        <Form.Radio label='Non, ce n’est pas nécessaire pour l’instant' checked={ this.props.form['sales-appointement'] === 'Non, ce n’est pas nécessaire pour l’instant' } />
       </Segment>
     </Form>;
   }
